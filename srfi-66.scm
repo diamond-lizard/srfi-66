c1a97b7da8ce6a16ab26b0bdd5b7e331743a70fd
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-11-10 11:39:20 -0800

    Created the srfi-66 module

--- srfi-66.scm
+++ srfi-66.scm
@@ -1,7 +1,22 @@
-(import scheme)
-(import (chicken base))
-(import (chicken platform))
+(module srfi-66
+    (
+     u8vector?
+     make-u8vector
+     u8vector
+     u8vector->list
+     list->u8vector
+     u8vector-length
+     u8vector-ref
+     u8vector-set!
+     u8vector=?
+     u8vector-compare
+     u8vector-copy!
+     u8vector-copy
+    )
+  (import scheme)
+  (import (chicken base))
+  (import (chicken platform))
 
-(register-feature! 'srfi-66)
+  (register-feature! 'srfi-66)
 
-(include "srfi-66-impl.scm")
+  (include "srfi-66-impl.scm"))
